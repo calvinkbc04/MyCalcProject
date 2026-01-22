5f3119e13bd9eb959b495d694236995f70054b5d
カール <138412347+calvinkbc04@users.noreply.github.com>
2026-01-22 14:35:29 +0900

    vs

---
+++ JenkinProject/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "calc.h"
+
+int main() {
+    std::cout << "10 + 5 = " << add(10, 5) << std::endl;
+    std::cout << "10 * 5 = " << mul(10, 5) << std::endl;
+    return 0;
+}
---
+++ JenkinProject/mul_div.cpp
@@ -0,0 +1,3 @@
+#include "calc.h"
+int mul(int a, int b) { return a * b; }
+double divi(int a, int b) { return (b != 0) ? (double)a / b : 0; }
